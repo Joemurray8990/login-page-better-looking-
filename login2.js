368617cd5c1516d047a2f9963bb067e6bc62b47e
Joemurray8990 <77066002+Joemurray8990@users.noreply.github.com>
2021-01-09 14:19:35 +0000

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,16 @@
+function validate()
+{
+var username=document.getElementById("username").nodeValue;
+var password=document.getElementById("password").nodeValue;
+if(username=="admin"&& password=="user")
+{
+    alert("login succesfully");
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+
+}
